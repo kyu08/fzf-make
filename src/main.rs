99d7dd60d0230b40e77fb70ea2feb45deb2d027d
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-03-11 01:41:02 +0900

    preview_command

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,26 @@ use skim::prelude::*;
 use std::io::Cursor;
 
 pub fn main() {
+    let preview_command = "bat --style=numbers --color=always --highlight-line $(bat Makefile | rg -n {}: | sed -e 's/:.*//g') Makefile";
     let options = SkimOptionsBuilder::default()
         .height(Some("50%"))
         .multi(true)
+        .preview(Some(preview_command))
         .build()
         .unwrap();
 
-    let input = "aaaaa\nbbbb\nccc".to_string();
+    // TODO: これをcommand一覧にする
+    let input = "PHONY\nrun\nclear".to_string();
 
-    // `SkimItemReader` is a helper to turn any `BufRead` into a stream of `SkimItem`
-    // `SkimItem` was implemented for `AsRef<str>` by default
     let item_reader = SkimItemReader::default();
     let items = item_reader.of_bufread(Cursor::new(input));
 
-    // `run_with` would read and show items from the stream
     let selected_items = Skim::run_with(&options, Some(items))
         .map(|out| out.selected_items)
-        .unwrap_or_else(|| Vec::new());
+        .unwrap_or_else(Vec::new);
 
     for item in selected_items.iter() {
-        print!("{}{}", item.output(), "\n");
+        // TODO: ここでmake hogeする
+        println!("{}", item.output());
     }
 }
