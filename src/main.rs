b66d9c60ff2b8c5023b2b1498b021ad824b74bcf
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-03-20 02:14:22 +0900

    fix extract command, add test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+use regex::Regex;
 use skim::prelude::*;
 
 use std::{fs::File, io::Cursor, io::Read, process};
 
-// TODO: add README
 fn main() {
     let (options, items) = get_params();
 
@@ -66,7 +66,7 @@ fn extract_command_from_makefile() -> Result<String, &'static str> {
         return Err("fail to read Makefile contents");
     }
 
-    let commands = extract_command(contents);
+    let commands = extract_commands(contents);
 
     if !commands.is_empty() {
         Ok(commands.join("\n"))
@@ -79,27 +79,46 @@ fn print_error(error_message: String) {
     println!("[ERR] {}", error_message);
 }
 
-fn extract_command(contents: String) -> Vec<String> {
+fn extract_commands(contents: String) -> Vec<String> {
     let mut result: Vec<String> = Vec::new();
     for line in contents.lines() {
-        if let Some(t) = line.split_once(':') {
-            if t.0.contains(".PHONY") {
-                continue;
-            }
-            result.push(t.0.to_string());
+        if let Some(c) = extract_command(line.to_string()) {
+            result.push(c);
         }
     }
 
     result
 }
 
+fn extract_command(line: String) -> Option<String> {
+    let regex = Regex::new(r"^(.*):$").unwrap();
+    match regex.find(line.as_str()) {
+        // TODO: もう少しいい書き方がありそう...
+        Some(m) => Some(
+            m.as_str()
+                .to_string()
+                .split_once(':')
+                .unwrap()
+                .0
+                .to_string(),
+        ),
+        None => None,
+    }
+}
+
 #[cfg(test)]
 mod test {
+    use std::str::FromStr;
+
     use super::*;
 
     #[test]
-    fn extract_command_test() {
-        let contents = "\
+    fn extract_commands_test() {
+        struct Case {
+            contents: String,
+            expect: Vec<String>,
+        }
+        let contents1 = "\
 .PHONY: run build check
 
 run:
@@ -113,10 +132,60 @@ check:
 
 echo:
 	@echo good";
+        let contents2 = "\
+.PHONY: close build
 
-        assert_eq!(
-            vec!["run", "build", "check", "echo"],
-            extract_command(contents.to_string())
-        );
+# https://example.com
+clone:
+		@git clone https://example.com
+
+build:
+		@cargo build";
+
+        let cases = vec![
+            Case {
+                contents: contents1.to_string(),
+                expect: vec![
+                    "run".to_string(),
+                    "build".to_string(),
+                    "check".to_string(),
+                    "echo".to_string(),
+                ],
+            },
+            Case {
+                contents: String::from_str(contents2).unwrap(),
+                expect: vec!["clone".to_string(), "build".to_string()],
+            },
+        ];
+
+        for case in cases {
+            assert_eq!(case.expect, extract_commands(case.contents));
+        }
+    }
+
+    #[test]
+    fn extract_command_test() {
+        struct Case {
+            contents: String,
+            expect: Option<String>,
+        }
+        let cases = vec![
+            Case {
+                contents: "echo:".to_string(),
+                expect: Some("echo".to_string()),
+            },
+            Case {
+                contents: "echo".to_string(),
+                expect: None,
+            },
+            Case {
+                contents: "https://example.com".to_string(),
+                expect: None,
+            },
+        ];
+
+        for case in cases {
+            assert_eq!(case.expect, extract_command(case.contents));
+        }
     }
 }
