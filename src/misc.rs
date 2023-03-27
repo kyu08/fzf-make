64ade9680fd57c00600ab16f5cf443e6f5adb8c1
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-03-28 01:41:37 +0900

    add comment

--- src/misc.rs
+++ src/misc.rs
@@ -13,8 +13,10 @@ pub fn print_error(error_message: String) {
     println!("[ERR] {}", error_message);
 }
 
+// TODO: Maybe skim related could be combined into one module.
 pub fn get_params<'a>() -> (SkimOptions<'a>, Option<Receiver<Arc<dyn SkimItem>>>) {
     // TODO: use cat when bat is unavailable
+    // fix #10 https://github.com/kyu08/fzf-make/issues/10
     let preview_command = "bat --style=numbers --color=always --highlight-line $(bat Makefile | grep -nE '^{}' | sed -e 's/:.*//g') Makefile";
     // TODO: hide fzf window when fzf-make terminated
     let options = SkimOptionsBuilder::default()
