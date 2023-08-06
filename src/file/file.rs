179f3d943928fad820655a6e87d937e45ee4fef7
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-08-07 01:01:30 +0900

    refactor

--- src/file/file.rs
+++ src/file/file.rs
@@ -1,5 +1,5 @@
 // module for file manipulation
-use std::{fs::OpenOptions, io::Read, path::Path};
+use std::path::Path;
 
 use crate::parser::{self, makefile};
 
@@ -9,35 +9,9 @@ pub fn create_makefile() -> Result<makefile::Makefile, &'static str> {
 
     Ok(parser::makefile::Makefile::new(
         Path::new(&makefile_name).to_path_buf(),
-    ))
+    )?)
 }
 
-pub fn concat_file_contents(file_paths: Vec<String>) -> Result<String, &'static str> {
-    let mut contents = String::new();
-    for path in file_paths {
-        let mut content = String::new();
-        // TODO: commonize convert file path to file content
-        let mut file = match OpenOptions::new().read(true).open(path) {
-            Err(_) => return Err("fail to open file"),
-            Ok(f) => f,
-        };
-
-        match file.read_to_string(&mut content) {
-            Err(e) => {
-                print!("fail to read file: {:?}", e);
-                return Err("fail to read file");
-            }
-            Ok(_) => {
-                if !contents.is_empty() {
-                    contents += "\n";
-                }
-
-                contents += &content;
-            }
-        }
-    }
-    Ok(contents)
-}
 fn specify_makefile_name(target_path: String) -> Option<String> {
     //  By default, when make looks for the makefile, it tries the following names, in order: GNUmakefile, makefile and Makefile.
     //  https://www.gnu.org/software/make/manual/make.html#Makefile-Names
@@ -59,97 +33,7 @@ fn specify_makefile_name(target_path: String) -> Option<String> {
 mod test {
     use super::*;
     use std::fs::{self, File};
-    use std::{io::Write, process};
     use uuid::Uuid;
-
-    #[test]
-    fn concat_file_contents_test() {
-        struct Case {
-            title: &'static str,
-            file_contents: Vec<&'static str>,
-            expect: Result<&'static str, &'static str>,
-        }
-        let cases = vec![
-            Case {
-                title: "two files",
-                file_contents: vec![
-                    "\
-.PHONY: test-1
-test-1:
-    @cargo run",
-                    "\
-.PHONY: test-2
-test-2:
-    @cargo run",
-                ],
-                expect: Ok("\
-.PHONY: test-1
-test-1:
-    @cargo run
-.PHONY: test-2
-test-2:
-    @cargo run"),
-            },
-            Case {
-                title: "single file",
-                file_contents: vec![
-                    "\
-.PHONY: test-1
-test-1:
-    @cargo run",
-                ],
-                expect: Ok("\
-.PHONY: test-1
-test-1:
-    @cargo run"),
-            },
-        ];
-
-        for case in cases {
-            let in_file_names: Vec<String> = case
-                .file_contents
-                .iter()
-                .map(|content| {
-                    let random_file_name = Uuid::new_v4().to_string();
-                    test_file_from_content(random_file_name, content)
-                })
-                .collect();
-
-            assert_eq!(
-                case.expect.map(|e| e.to_string()),
-                concat_file_contents(in_file_names),
-                "\nFailed in the 🚨{:?}🚨",
-                case.title,
-            );
-        }
-    }
-
-    fn test_file_from_content(file_name: String, content: &'static str) -> String {
-        let tmp_dir = std::env::temp_dir();
-        let file_name = file_name + ".mk";
-        let file_path = tmp_dir.join(&file_name);
-
-        let mut file = match OpenOptions::new()
-            .create(true)
-            .write(true)
-            .read(true)
-            .append(true)
-            .open(&file_path)
-        {
-            Err(err) => panic!("fail to create file: {:?}", err),
-            Ok(file) => file,
-        };
-
-        match file.write_all(content.as_bytes()) {
-            Err(e) => {
-                print!("fail to write file: {:?}", e);
-                process::exit(1);
-            }
-            Ok(_) => {}
-        }
-
-        file_path.to_path_buf().to_str().unwrap().to_string()
-    }
 
     #[test]
     fn specify_makefile_name_test() {
--- src/parser/makefile.rs
+++ src/parser/makefile.rs
@@ -14,41 +14,196 @@ pub struct Makefile {
 }
 
 impl Makefile {
-    pub fn new(path: PathBuf) -> Makefile {
-        let file_content = Makefile::patn_to_content(path.clone());
+    // TODO: add UT
+    pub fn new(path: PathBuf) -> Result<Makefile, &'static str> {
+        let file_content = Makefile::path_to_content(path.clone());
         let including_file_paths = include::extract_including_file_paths(file_content.clone());
-        let include_files = including_file_paths
+        let include_files: Vec<Result<Makefile, &'static str>> = including_file_paths
             .iter()
             .map(|path| Makefile::new(Path::new(&path).to_path_buf()))
             .collect();
-        let targets = target::content_to_commands(file_content).unwrap();
 
-        Makefile {
+        // この辺のエラーの扱いどうしよう
+        // TODO: extract as fn?
+        let mut include_files_err: Vec<&'static str> = vec![];
+        let mut include_files_ok: Vec<Makefile> = vec![];
+        for i in include_files {
+            match i {
+                Ok(m) => include_files_ok.push(m),
+                Err(err) => include_files_err.push(err),
+            }
+        }
+
+        let targets = target::content_to_commands(file_content);
+        if let Err(e) = targets {
+            print!("failed to parse target");
+            return Err(e);
+        }
+
+        Ok(Makefile {
             path,
-            include_files,
-            targets,
+            include_files: include_files_ok,
+            targets: targets.unwrap(),
+        })
+    }
+
+    pub fn to_include_path_string(&self) -> Vec<String> {
+        let mut result: Vec<String> = vec![];
+        result.push(self.path.to_string_lossy().to_string());
+
+        for include_file in &self.include_files {
+            Vec::append(&mut result, &mut include_file.to_include_path_string());
+        }
+
+        result
+    }
+
+    pub fn to_target_string(&self) -> Vec<String> {
+        let mut result: Vec<String> = vec![];
+        (&mut result).append(&mut self.targets.clone());
+        for include_file in &self.include_files {
+            Vec::append(&mut result, &mut include_file.to_target_string());
+        }
+
+        result
+    }
+
+    // TODO: add UT
+    fn path_to_content(path: PathBuf) -> String {
+        let mut content = String::new();
+        let mut f = File::open(&path).unwrap();
+        f.read_to_string(&mut content).unwrap();
+
+        content
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn makefile_to_include_path_string_test() {
+        struct Case {
+            title: &'static str,
+            makefile: Makefile,
+            expect: Vec<&'static str>,
+        }
+
+        let cases = vec![
+            Case {
+                title: "makefile with no include directive",
+                makefile: Makefile {
+                    path: Path::new("path").to_path_buf(),
+                    include_files: vec![],
+                    targets: vec!["test".to_string(), "run".to_string()],
+                },
+                expect: vec!["path"],
+            },
+            Case {
+                title: "makefile with nested include directive",
+                makefile: Makefile {
+                    path: Path::new("path1").to_path_buf(),
+                    include_files: vec![
+                        Makefile {
+                            path: Path::new("path2").to_path_buf(),
+                            include_files: vec![Makefile {
+                                path: Path::new("path2-1").to_path_buf(),
+                                include_files: vec![],
+                                targets: vec![],
+                            }],
+                            targets: vec![],
+                        },
+                        Makefile {
+                            path: Path::new("path3").to_path_buf(),
+                            include_files: vec![],
+                            targets: vec![],
+                        },
+                    ],
+                    targets: vec![],
+                },
+                expect: vec!["path1", "path2", "path2-1", "path3"],
+            },
+        ];
+
+        for case in cases {
+            let mut expect_string: Vec<String> =
+                case.expect.iter().map(|e| e.to_string()).collect();
+            expect_string.sort();
+            let sorted_result = case.makefile.to_include_path_string();
+
+            assert_eq!(
+                expect_string, sorted_result,
+                "\nFailed in the 🚨{:?}🚨",
+                case.title,
+            )
         }
     }
 
-    // TODO: add UT
-    pub fn to_include_path_string(&self) -> Vec<String> {
-        self.include_files
-            .iter()
-            .map(|m| m.path.to_string_lossy().to_string())
-            .collect()
-    }
+    #[test]
+    fn makefile_to_target_string_test() {
+        struct Case {
+            title: &'static str,
+            makefile: Makefile,
+            expect: Vec<&'static str>,
+        }
 
-    // TODO: add UT
-    pub fn to_target_string(&self) -> Vec<String> {
-        self.targets.clone()
-    }
+        let cases = vec![
+            Case {
+                title: "makefile with no target",
+                makefile: Makefile {
+                    path: Path::new("path").to_path_buf(),
+                    include_files: vec![],
+                    targets: vec![],
+                },
+                expect: vec![],
+            },
+            Case {
+                title: "makefile with no include directive",
+                makefile: Makefile {
+                    path: Path::new("path").to_path_buf(),
+                    include_files: vec![],
+                    targets: vec!["test".to_string(), "run".to_string()],
+                },
+                expect: vec!["test", "run"],
+            },
+            Case {
+                title: "makefile with nested include directive",
+                makefile: Makefile {
+                    path: Path::new("path1").to_path_buf(),
+                    include_files: vec![
+                        Makefile {
+                            path: Path::new("path2").to_path_buf(),
+                            include_files: vec![Makefile {
+                                path: Path::new("path2-1").to_path_buf(),
+                                include_files: vec![],
+                                targets: vec!["test2-1".to_string(), "run2-1".to_string()],
+                            }],
+                            targets: vec!["test2".to_string(), "run2".to_string()],
+                        },
+                        Makefile {
+                            path: Path::new("path3").to_path_buf(),
+                            include_files: vec![],
+                            targets: vec!["test3".to_string(), "run3".to_string()],
+                        },
+                    ],
+                    targets: vec!["test1".to_string(), "run1".to_string()],
+                },
+                expect: vec![
+                    "test1", "run1", "test2", "run2", "test2-1", "run2-1", "test3", "run3",
+                ],
+            },
+        ];
 
-    fn patn_to_content(path: PathBuf) -> String {
-        let mut contents = String::new();
-        // TODO: handling error of result
-        let mut f = File::open(&path).unwrap();
-        // TODO: handling error of result
-        f.read_to_string(&mut contents).unwrap();
-        contents
+        for case in cases {
+            let expect_string: Vec<String> = case.expect.iter().map(|e| e.to_string()).collect();
+
+            assert_eq!(
+                expect_string,
+                case.makefile.to_target_string(),
+                "\nFailed in the 🚨{:?}🚨",
+                case.title,
+            )
+        }
     }
 }
