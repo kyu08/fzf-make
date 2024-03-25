6e6e5345afb7ccec3faa20ddb247a9791d02e61d
Sigmanificient <edhyjox@gmail.com>
2024-03-25 22:10:36 +0100

    Add missing tools in dev shell

--- flake.nix
+++ flake.nix
@@ -16,6 +16,7 @@
       {
         devShells.default = pkgs.mkShell {
           inputsFrom = [ packages.fzf-make ];
+          packages = with pkgs; [ clippy typos ];
         };
 
         formatter = pkgs.nixpkgs-fmt;
