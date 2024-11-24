8ed1f59a436f38744265650a5677de87e78d660c
kyu08 <49891479+kyu08@users.noreply.github.com>
2024-11-24 15:30:56 +0900

    change format to display command: (runner_type) name -> [runner_type] name]

--- src/model/command.rs
+++ src/model/command.rs
@@ -12,13 +12,13 @@ pub struct Command {
 impl Command {
     pub fn new(
         runner_type: runner_type::RunnerType,
-        command_name: String,
+        name: String,
         file_name: PathBuf,
         line_number: u32,
     ) -> Self {
         Self {
             runner_type,
-            name: command_name,
+            name,
             file_name,
             line_number,
         }
@@ -27,6 +27,6 @@ impl Command {
 
 impl fmt::Display for Command {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}) {}", self.runner_type, self.name)
+        write!(f, "[{}] {}", self.runner_type, self.name)
     }
 }
