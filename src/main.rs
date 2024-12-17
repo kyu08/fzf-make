9e5b7bd95e380e3f73d4cd7c712eabb135bdf23f
kyu08 <49891479+kyu08@users.noreply.github.com>
2024-12-18 00:31:43 +0900

    fix main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,10 @@ use std::panic;
 
 #[tokio::main]
 async fn main() {
-    // let result = panic::catch_unwind(|| async {
-    // controller::controller_main::run().await;
-    // });
-    // if let Err(e) = result.await {
-    controller::controller_main::run().await;
-    // if let Err(e) = .await.unwrap() {
-    //     println!("{}", err::any_to_string::any_to_string(&*e));
-    //     std::process::exit(1);
-    // }
+    let result =
+        panic::catch_unwind(|| tokio::spawn(async { controller::controller_main::run().await }));
+    if let Err(e) = result {
+        println!("{}", err::any_to_string::any_to_string(&*e));
+        std::process::exit(1);
+    }
 }
