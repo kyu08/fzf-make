4a53e87394ec2325e33e8f091f9c53787ecb1619
kyu08 <49891479+kyu08@users.noreply.github.com>
2024-12-17 23:52:13 +0900

    wip

--- src/usecase/usecase_main.rs
+++ src/usecase/usecase_main.rs
@@ -4,6 +4,6 @@ use anyhow::Result;
 
 pub trait Usecase {
     fn command_str(&self) -> Vec<&'static str>;
-    fn run(&self) -> impl Future<Output = Result<(), anyhow::Error>>;
+    async fn run(&self) -> Result<(), anyhow::Error>;
     // impl Future<Output = Result<(), anyhow::Error>>
 }
