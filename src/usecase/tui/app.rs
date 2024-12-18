55c62a038851a137adcbefca1b372bacd1a45c06
kyu08 <49891479+kyu08@users.noreply.github.com>
2024-12-19 02:27:23 +0900

    update `CREDITS`

--- src/usecase/tui/app.rs
+++ src/usecase/tui/app.rs
@@ -265,8 +265,9 @@ fn shutdown_terminal(terminal: &mut Terminal<CrosstermBackend<Stderr>>) -> Resul
 async fn get_latest_version(share_clone: Arc<Mutex<HashMap<String, String>>>) {
     let pkg_name = "kyu08/fzf-make";
     let current_version = env!("CARGO_PKG_VERSION").to_string();
-    let informer = update_informer::new(registry::GitHub, pkg_name, current_version)
-        .interval(Duration::from_secs(60 * 60 * 24)); // check version once a day
+    let informer =
+        update_informer::new(registry::GitHub, pkg_name, current_version).interval(Duration::ZERO); // check version once a day
+                                                                                                    // .interval(Duration::from_secs(60 * 60 * 24)); // check version once a day
     let version_result =
         task::spawn_blocking(|| informer.check_version().map_err(|e| e.to_string()))
             .await
