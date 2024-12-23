611cf38d0652af253184d891a7881e5bc64aa72d
kyu08 <49891479+kyu08@users.noreply.github.com>
2024-12-24 00:16:57 +0900

    find_justfile

--- src/model/just/just.rs
+++ src/model/just/just.rs
@@ -8,17 +8,12 @@ pub struct Just {
     commands: Vec<command::Command>,
 }
 impl Just {
-    // TODO: add new
     pub fn new(current_dir: PathBuf) -> Result<Just> {
-        // TODO: justではjustfileの子ディレクトリでもjust testのように実行できる。
-        // 子ディレクトリでもfzf-makeを実行できるためにはjustfileのパスを取得する必要がある。
-        // 現状justコマンドでこれをする方法が見つからなかったため、親方向にgit rootまで調べるくらいしか方法がないかもしれない。(git管理されてなかったらエラーにする)
-        //
-        // あとはひとまずjustfileが存在するディレクトリでの実行だけをサポートして、それから子ディレクトリでの実行をサポートするという手があるかも知れない。
-        //
-        // just --dumpでjustfileの内容を取得
+        let justfile_path = match Just::find_justfile(current_dir.clone()) {
+            Some(path) => path,
+            None => bail!("justfile not found"),
+        };
         // tree-sitterを使ってパースしつつ行番号を取得
-        // tmp_fileに保存してそのpathをcommandに格納する
         bail!("not implemented")
     }
 
@@ -66,15 +61,21 @@ impl Just {
             .map(|_| command)
     }
 
-    fn find_justfile(current_dir: &PathBuf) -> Option<PathBuf> {
-        // TODO: current_dirの親ディレクトリを取得
-        // NOTE: current_dirは絶対パス
-        let parent_dir = current_dir.ancestors()?;
-        // TODO: justfileが見つかるまで子 -> 親方向に走査
+    fn find_justfile(current_dir: PathBuf) -> Option<PathBuf> {
+        for path in current_dir.ancestors() {
+            for entry in PathBuf::from(path).read_dir().unwrap() {
+                let entry = entry.unwrap();
+                let file_name = entry.file_name().to_string_lossy().to_lowercase();
+                if file_name == "justfile" || file_name == ".justfile" {
+                    return Some(entry.path());
+                }
+            }
+        }
         None
     }
 }
 
+#[cfg(test)]
 mod test {
     use super::*;
     use uuid::Uuid;
@@ -89,14 +90,44 @@ mod test {
 
         // justfile exists in temp_dir
         {
-            let random_dir_name = Uuid::new_v4().to_string();
-            let test_target_dir = test_root_dir.join(random_dir_name);
+            let test_target_dir = test_root_dir.join(Uuid::new_v4().to_string());
             std::fs::create_dir(&test_target_dir).unwrap();
 
             let justfile_path = test_target_dir.join("justfile");
             std::fs::File::create(&justfile_path).unwrap();
-            assert_eq!(Just::find_justfile(&test_target_dir), Some(justfile_path));
+            assert_eq!(Just::find_justfile(test_target_dir), Some(justfile_path));
         }
+
+        // .justfile exists in temp_dir
+        {
+            let test_target_dir = test_root_dir.join(Uuid::new_v4().to_string());
+            std::fs::create_dir(&test_target_dir).unwrap();
+
+            let justfile_path = test_target_dir.join(".justfile");
+            std::fs::File::create(&justfile_path).unwrap();
+            assert_eq!(Just::find_justfile(test_target_dir), Some(justfile_path));
+        }
+
+        // justfile exists in the one of ancestors of temp_dir
+        {
+            let parent = test_root_dir.join(Uuid::new_v4().to_string());
+            let test_target_dir = parent.join("child_dir");
+            std::fs::create_dir_all(&test_target_dir).unwrap();
+
+            let justfile_path = parent.join("justfile");
+            std::fs::File::create(&justfile_path).unwrap();
+            assert_eq!(Just::find_justfile(test_target_dir), Some(justfile_path));
+        }
+
+        // no justfile exists
+        {
+            let parent = test_root_dir.join(Uuid::new_v4().to_string());
+            let test_target_dir = parent.join("child_dir");
+            std::fs::create_dir_all(&test_target_dir).unwrap();
+
+            assert_eq!(Just::find_justfile(test_target_dir), None);
+        }
+
         let _ = std::fs::remove_dir_all(&test_root_dir);
     }
 }
