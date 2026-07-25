a45906ccd6b54f1a63feab78a7396066e492ae72
kyu08 <49891479+kyu08@users.noreply.github.com>
2026-07-25 09:59:08 +0900

    fix(file_util): handle file related errors properly

--- src/model/file_util.rs
+++ src/model/file_util.rs
@@ -11,8 +11,16 @@ pub fn path_to_content(path: PathBuf) -> Result<String> {
 
 pub fn find_file_in_ancestors(current_dir: PathBuf, file_names: Vec<&str>) -> Option<PathBuf> {
     for path in current_dir.ancestors() {
-        for entry in PathBuf::from(path).read_dir().unwrap() {
-            let entry = entry.unwrap();
+        let read_dir = match PathBuf::from(path).read_dir() {
+            Ok(r) => r,
+            Err(_) => continue,
+        };
+
+        for entry in read_dir {
+            let entry = match entry {
+                Ok(e) => e,
+                Err(_) => continue,
+            };
             let file_name = entry.file_name().to_string_lossy().to_lowercase();
             if file_names.contains(&file_name.as_str()) {
                 return Some(entry.path());
